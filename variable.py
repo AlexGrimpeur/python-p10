d1b72a302aaf7f76d5a117f3b2af2d7396b19daf
AlexGrimpeur <moreaualexandre2002@gmail.com>
2022-11-29 11:54:47 +0100

    ajout de variable de str test modif de commit avec 'git commit --amend'

--- variable.py
+++ variable.py
@@ -1,8 +1,14 @@
 
 
-my_text1 = "ceci est une chaîne de caractère"
-my_text2 = 'ceci est aussi une chaîne de caractère'
+# l'opérateur d'affectation = permet d'affecter une valeur dans une valeur 
+my_text1 = "ceci est une chaîne de caractères"
+my_text2 = 'ceci est aussi une chaîne de caractères'
 
 print(my_text1)
 print(my_text2)
+
+my_number1 = 142
+my_number2 = -65
+
+print(my_number1 - my_number2)
 
