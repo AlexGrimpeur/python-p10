b4fbe58f162fa2b4cea033c281f9127df8abdfb3
AlexGrimpeur <moreaualexandre2002@gmail.com>
2022-12-05 13:40:29 +0100

    suite du cour et de l'apprentissage sur python

--- variable.py
+++ variable.py
@@ -3,12 +3,57 @@
 # l'opérateur d'affectation = permet d'affecter une valeur dans une valeur 
 my_text1 = "ceci est une chaîne de caractères"
 my_text2 = 'ceci est aussi une chaîne de caractères'
+my_text3 = "j'aime les pommes\nmais asssi les poires"
 
 print(my_text1)
 print(my_text2)
+print(my_text3)
 
+# integer, nombre entier 
 my_number1 = 142
 my_number2 = -65
 
 print(my_number1 - my_number2)
 
+# float, nombre a virgule flottante 
+my_number3 = 3.14
+my_number4 = -2.71
+my_number5 = 0.0 
+
+print(my_number3)
+print(my_number4)
+print(my_number5)
+
+print(type(my_number5))
+
+# les booleans 
+
+my_boolean1 = True
+my_boolean2 = False
+print(my_boolean1)
+print(my_boolean2)
+print(type(my_boolean1))
+
+# valeur nulle 
+
+my_none = None 
+print(my_none)
+print(type(my_none))
+
+# permutation de la valeur des variables 
+
+"""a = 123 
+b = 42
+a, b = b, a 
+
+print(a, b)"""
+
+a = 10
+b = 2 
+
+c = a 
+a = b
+b = c
+print(a, b)
+
+# ctrl K T = ouvrir la gestion des thèmes 
