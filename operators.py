90ec69c4acc24d6a757ecc619c0690e369c9f8b4
AlexGrimpeur <moreaualexandre2002@gmail.com>
2022-12-05 14:29:24 +0100

    ...

---
+++ operators.py
@@ -0,0 +1,11 @@
+import random
+
+foo = random.randint(1,100)
+print(foo)
+print(foo % 2)
+foo != 0 
+
+if foo == True :
+    print("foo est un nombre pair")
+elif foo == False :
+    print("foo est un nombre impair")
